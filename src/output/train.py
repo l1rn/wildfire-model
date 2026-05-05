f0d1429680685bd4750d110e13ffd428c35c8330
l1rn <ivtaks06@gmail.com>
2026-05-05 22:10:47 +0500

    update pdp analysis

--- src/output/train.py
+++ src/output/train.py
@@ -185,32 +185,58 @@ def generate_partial_dependence_plots(model, X_test, sample_size=10000, random_s
     
     X_eval = X_test
         
-    features_to_plot = [
+    features_1d = [
         'ghm', 
         'vpd', 
-        ('ghm', 'vpd')
+        'ndiv',
     ]
     
+
     fig, ax = plt.subplots(figsize=(15, 6))
     
-    display = PartialDependenceDisplay.from_estimator(
+    display_1d = PartialDependenceDisplay.from_estimator(
         estimator=model,
         X=X_eval,
-        features=features_to_plot,
+        features=features_1d,
         kind='average',
         grid_resolution=40, 
         ax=ax,
-        n_jobs=-1
+        n_jobs=-1,
+        n_cols=3,
     )
     
-    fig.suptitle('Partial Dependence: Infrastructure Proximity vs. Synergistic Climate Effects', fontsize=16)
-    plt.subplots_adjust(top=0.9)  
+    fig_1d = display_1d.figure_
+    fig_1d.set_size_inches(16, 6)
+    display_1d.figure_.suptitle("1D Partial Dependence Plots", fontsize=16)
+
+    output_filename_1d = Path(PROCESSED_DIR) / "pdp_1d.png"
+    plt.savefig(output_filename_1d, dpi=300, bbox_inches='tight')
+    plt.close(fig_1d)
     
-    output_filename = Path(PROCESSED_DIR) / "pdp_infrastructure_climate.png"
-    plt.savefig(output_filename, dpi=300, bbox_inches='tight')
-    plt.close()
+    print(f"Partial Dependence Plots saved successfully to {output_filename_1d}")
     
-    print(f"Partial Dependence Plots saved successfully to {output_filename}")
+    features_2d = [
+        ('ghm', 'vpd'),
+        ('ghm', 'ndvi'),
+        ('vpd', 'ndvi'),
+    ]
+    
+    fig_2d = display_2d.figure_
+    fig_2d.set_size_inches(16, 6)
+    display_2d = PartialDependenceDisplay.from_estimator(
+        estimator=model,
+        X=X_eval,
+        features=features_2d,
+        kind='average',
+        grid_resolution=40, 
+        ax=ax,
+        n_jobs=-1,
+        n_cols=3,
+    )
+    
+    output_filename_2d = Path(PROCESSED_DIR) / "pdp_2d.png"
+    plt.savefig(output_filename_2d, dpi=300, bbox_inches='tight')
+    plt.close(fig_2d)
     
 def generate_forecast(
     model, 
